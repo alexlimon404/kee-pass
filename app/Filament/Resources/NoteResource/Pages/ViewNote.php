121dd287c3074ccfcf87be03f30ffe8150bb3526
alexlimon404 <mirrorbitmirror@gmail.com>
2025-06-06 01:28:18 +0300

    fix

--- app/Filament/Resources/NoteResource/Pages/ViewNote.php
+++ app/Filament/Resources/NoteResource/Pages/ViewNote.php
@@ -14,6 +14,7 @@ class ViewNote extends ViewRecord
     {
         return [
             Actions\EditAction::make()->badge(),
+            Actions\DeleteAction::make()->badge(),
         ];
     }
 }
